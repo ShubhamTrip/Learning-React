7f3b78545a62f9219b6bac3479daa62add1bda0b
Shubham Tripathi <shubhamtrip16@gmail.com>
2022-10-06 22:07:00 +0530

    changes made

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import FunctionsComp from './FuncComponents.js';
+import ClassComp from './ClassComp.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FunctionsComp txt='Hello' obj={{name:'Shubham', roll:'dev'}}/>        {/* FUnctional Components  with passing props*/}
+      <ClassComp txt='This is an Example of class Component'/>
     </div>
   );
 }
